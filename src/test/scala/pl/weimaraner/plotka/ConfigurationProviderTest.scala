20755bbcb96c3d133c6b11bfb04057001f44ea29
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-04-30 19:13:22 +0200

    Add initial configuration.

--- src/test/scala/pl/weimaraner/plotka/ConfigurationProviderTest.scala
+++ src/test/scala/pl/weimaraner/plotka/ConfigurationProviderTest.scala
@@ -1,9 +1,12 @@
 package pl.weimaraner.plotka
 
+import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.FunSuite
 
 class ConfigurationProviderTest extends FunSuite {
-  test("Should load listener settings") {
-
+  test("Should load node settings") {
+    val loadedConfiguration = ConfigFactory.load("test_configuration")
+    assert(loadedConfiguration.hasPath("node"), "There is no configuration for node")
   }
 }
+
