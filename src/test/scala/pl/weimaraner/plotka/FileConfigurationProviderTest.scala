ec09c991a6a8e8d9a0ea2d8eefcc31d882a77fa0
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-06-19 21:12:12 +0200

    Autoformat + remove new for case classes.

--- src/test/scala/pl/weimaraner/plotka/FileConfigurationProviderTest.scala
+++ src/test/scala/pl/weimaraner/plotka/FileConfigurationProviderTest.scala
@@ -7,15 +7,15 @@ import pl.weimaraner.plotka.conf.providers.FileConfigurationProvider
 class FileConfigurationProviderTest extends FunSuite with Matchers {
   private val configurationFile = "test_configuration"
   private val expectedPeerConfiguration = List(
-    new BasicPeerConfiguration("node1.network", 2048),
-    new BasicPeerConfiguration("node2.network")
+    BasicPeerConfiguration("node1.network", 2048),
+    BasicPeerConfiguration("node2.network")
   )
-  private val expectedConfiguration = new BasicNodeConfiguration("node0.network", 2828, "10.0.0.1", expectedPeerConfiguration)
+  private val expectedConfiguration = BasicNodeConfiguration("node0.network", 2828, "10.0.0.1", expectedPeerConfiguration)
 
   test("Should load correct values from configuration file") {
     val fileConfigurationProvider = new FileConfigurationProvider(configurationFile)
     val actualConfiguration = fileConfigurationProvider.loadConfiguration
-    actualConfiguration should equal (expectedConfiguration)
+    actualConfiguration should equal(expectedConfiguration)
   }
 }
 
