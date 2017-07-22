c8a349b495d9ecf55d9c80621d19613f6579ea24
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-07-22 10:18:57 +0200

    Add test for StandardNetworkMessageConsumer

--- src/main/scala/pl/weimaraner/plotka/network/consumer/StandardNetworkMessageConsumer.scala
+++ src/main/scala/pl/weimaraner/plotka/network/consumer/StandardNetworkMessageConsumer.scala
@@ -7,16 +7,16 @@ import pl.weimaraner.plotka.network.talker.{NetworkTalker, Talker}
   * This basic network message consumer composes NetworkTalker to allow
   * communication in both directions. The localPeer definition is
   * used as a sender identification during messaging. Message handler
-  * is provided with incomming message and also instance of Talker
+  * is provided with incoming message and also instance of Talker
   * that can be used to send a response.
   *
-  * @param localPeer local peer definition used by talker
+  * @param localPeer      local peer definition used by talker
   * @param messageHandler function to be executed when message arrives
   */
 class StandardNetworkMessageConsumer(localPeer: Peer,
-                                          messageHandler: (NetworkMessage, Talker) => Unit
-                                         ) extends NetworkMessageConsumer {
-  private val talker : Talker = new NetworkTalker(localPeer)
+                                     messageHandler: (NetworkMessage, Talker) => Unit)
+  extends NetworkMessageConsumer {
+  private val talker: Talker = new NetworkTalker(localPeer)
 
   override def consumeMessage(message: Message[NetworkPeer, Peer, Serializable]): Unit = {
     consumeNetworkMessage(message.asInstanceOf[NetworkMessage])
