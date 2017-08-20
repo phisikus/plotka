b1c1b925f49b86b030c6e057eeb8ca657af65795
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-08-20 10:20:37 +0200

    Improve SimpleExample.

--- javaexample/src/main/java/SimpleExample.java
+++ javaexample/src/main/java/SimpleExample.java
@@ -1,39 +1,40 @@
-import eu.phisikus.plotka.conf.NodeConfiguration;
-import eu.phisikus.plotka.conf.model.BasicNodeConfiguration;
-import eu.phisikus.plotka.conf.model.BasicPeerConfiguration;
 import eu.phisikus.plotka.model.NetworkPeer;
-import eu.phisikus.plotka.model.Peer;
+import eu.phisikus.plotka.network.consumer.StandardNetworkMessageConsumer;
 import eu.phisikus.plotka.network.listener.NetworkListener;
 import eu.phisikus.plotka.network.listener.NetworkListenerBuilder;
 import eu.phisikus.plotka.network.talker.NetworkTalker;
 import eu.phisikus.plotka.network.talker.Talker;
-import scala.collection.concurrent.BasicNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.runtime.BoxedUnit;
 
 public class SimpleExample {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleExample.class);
+
+    public static void main(String[] args) throws InterruptedException {
+
         NetworkPeer localPeer = new NetworkPeer("LocalPeer1", "127.0.0.1", 3030);
         Talker testTalker = new NetworkTalker(localPeer);
+
+        StandardNetworkMessageConsumer messageConsumer = new StandardNetworkMessageConsumer(localPeer, (message, talker) -> {
+            TextMessage textMessage = (TextMessage) message.getMessage();
+            String receviedText = textMessage.getText();
+            logger.info("All good! I've got the message: {}", receviedText);
+            return BoxedUnit.UNIT;
+        });
+
         NetworkListener testListener = NetworkListenerBuilder.apply()
                 .withId(localPeer.getId())
                 .withAddress(localPeer.getAddress())
                 .withPort(localPeer.getPort())
-                .withPeer(new BasicPeerConfiguration(localPeer.getAddress(), localPeer.getPort()))
-                .withMessageHandler(message -> {
-                })
+                .withMessageHandler(messageConsumer)
                 .build();
 
 
-        NodeConfiguration nodeConfiguration = new BasicNodeConfiguration("123", 123, "123", null);
-        Peer peer = new Peer("wef");
-
         testListener.start();
         testTalker.send(localPeer, new TextMessage("Hello!"));
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(1000L);
         testListener.stop();
     }
 
