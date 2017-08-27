c56bd424909fc54b878a346bef724b9a7a9c2337
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-08-27 09:31:52 +0200

    Add docs to config project.

--- config/src/main/scala/eu/phisikus/plotka/conf/NodeConfiguration.scala
+++ config/src/main/scala/eu/phisikus/plotka/conf/NodeConfiguration.scala
@@ -3,6 +3,9 @@ package eu.phisikus.plotka.conf
 import scala.beans.BeanProperty
 
 
+/**
+  * It represents node configuration describing network settings and initial list of other peers.
+  */
 trait NodeConfiguration {
   @BeanProperty def id: String
 
--- config/src/main/scala/eu/phisikus/plotka/conf/PeerConfiguration.scala
+++ config/src/main/scala/eu/phisikus/plotka/conf/PeerConfiguration.scala
@@ -2,6 +2,10 @@ package eu.phisikus.plotka.conf
 
 import scala.beans.BeanProperty
 
+/**
+  * It represents information required to connect to some peer in the network.
+  * List of those objects would be used in configuration (NodeConfiguration).
+  */
 trait PeerConfiguration {
   @BeanProperty def address: String
 
