8e9a6490e5b85d865262f29d905380d947970dba
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-09-10 10:58:16 +0200

    Extend FileConfigurationProvider to use the default config if no name is provided.

--- config/src/test/scala/eu/phisikus/plotka/conf/FileConfigurationProviderTest.scala
+++ config/src/test/scala/eu/phisikus/plotka/conf/FileConfigurationProviderTest.scala
@@ -1,5 +1,6 @@
 package eu.phisikus.plotka.conf
 
+import com.typesafe.config.ConfigException
 import eu.phisikus.plotka.conf.model.{BasicNodeConfiguration, BasicPeerConfiguration}
 import eu.phisikus.plotka.conf.providers.FileConfigurationProvider
 import org.scalatest.{FunSuite, Matchers}
@@ -13,9 +14,16 @@ class FileConfigurationProviderTest extends FunSuite with Matchers {
   private val expectedConfiguration = BasicNodeConfiguration("node0.network", 2828, "10.0.0.1", expectedPeerConfiguration)
 
   test("Should load correct values from configuration file") {
-    val fileConfigurationProvider = new FileConfigurationProvider(configurationFile)
+    val fileConfigurationProvider = new FileConfigurationProvider(Some(configurationFile))
     val actualConfiguration = fileConfigurationProvider.loadConfiguration
     actualConfiguration should equal(expectedConfiguration)
+  }
+
+  test("Should fail when configuration file does not exist") {
+    val fileConfigurationProvider = new FileConfigurationProvider()
+    assertThrows[ConfigException.Missing] {
+      fileConfigurationProvider.loadConfiguration
+    }
   }
 }
 
