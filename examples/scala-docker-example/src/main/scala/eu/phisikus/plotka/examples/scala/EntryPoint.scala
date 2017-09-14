6594a9ae28055d2814083333ff06cd116d034f0d
Marcin Biernacki <phisikus@platinum.linux.pl>
2017-09-14 23:37:33 +0200

    Improve concurrency in scala example.

--- examples/scala-docker-example/src/main/scala/eu/phisikus/plotka/examples/scala/EntryPoint.scala
+++ examples/scala-docker-example/src/main/scala/eu/phisikus/plotka/examples/scala/EntryPoint.scala
@@ -1,14 +1,13 @@
 package eu.phisikus.plotka.examples.scala
 
+import java.util.concurrent.ConcurrentLinkedQueue
+
 import com.typesafe.scalalogging.Logger
 import eu.phisikus.plotka.conf.NodeConfiguration
 import eu.phisikus.plotka.conf.providers.FileConfigurationProvider
-import eu.phisikus.plotka.model.NetworkPeer
-import eu.phisikus.plotka.network.consumer.StandardNetworkMessageConsumer
+import eu.phisikus.plotka.model.{NetworkMessage, NetworkMessageConsumer, NetworkPeer}
 import eu.phisikus.plotka.network.listener.{NetworkListener, NetworkListenerBuilder}
 import eu.phisikus.plotka.network.talker.NetworkTalker
-
-import scala.collection.mutable
 
 
 /**
@@ -17,21 +16,24 @@ import scala.collection.mutable
   */
 object EntryPoint {
   private val logger = Logger("EntryPoint")
+  private val peersThatResponded = new ConcurrentLinkedQueue[NetworkPeer]
+
+  /**
+    * Main logic here - incoming message handler stores the sender info in the queue
+    */
+  private val messageConsumer: NetworkMessageConsumer = incomingMessage => {
+    val message = incomingMessage.asInstanceOf[NetworkMessage]
+    val textMessage = message.getMessage.asInstanceOf[TextMessage]
+    val sender = message.sender.asInstanceOf[NetworkPeer]
+    val receivedText = textMessage.text
+    logger.info("I have received message from {} with content = {}", sender, receivedText)
+    peersThatResponded.add(sender)
+  }
 
 
   def main(args: Array[String]): Unit = {
-    val configurationProvider: FileConfigurationProvider = new FileConfigurationProvider()
-    val configuration = configurationProvider.loadConfiguration
-    val peersThatResponded = new mutable.MutableList[NetworkPeer]
-
+    val configuration: NodeConfiguration = getConfiguration
     val localPeer: NetworkPeer = NetworkPeer(configuration.id, configuration.address, configuration.port)
-    val messageConsumer = new StandardNetworkMessageConsumer(localPeer, (message, talker) => {
-      val textMessage = message.getMessage.asInstanceOf[TextMessage]
-      val sender = message.sender.asInstanceOf[NetworkPeer]
-      val receivedText = textMessage.text
-      logger.info("I have received message from {} with content = {}", sender, receivedText)
-      peersThatResponded += sender
-    })
 
     val testListener: NetworkListener = buildListener(localPeer, messageConsumer)
 
@@ -39,21 +41,35 @@ object EntryPoint {
 
     sendHelloToAllPeers(configuration, localPeer)
     waitForAllResponses(configuration, peersThatResponded)
-    val listOfPeersThatResponded = peersThatResponded.map(localPeer => localPeer.id).mkString(",")
-    logger.info("Received replies from: {}", listOfPeersThatResponded)
+
+    printRespondingPeerList()
 
     testListener.stop()
 
   }
 
-  private def waitForAllResponses(configuration: NodeConfiguration, peersThatResponded: mutable.MutableList[NetworkPeer]) = {
+  private def printRespondingPeerList() = {
+    val listOfPeersThatResponded = peersThatResponded
+      .toArray(Array[NetworkPeer]())
+      .map(peer => peer.id)
+      .mkString(",")
+    logger.info("Received replies from: {}", listOfPeersThatResponded)
+  }
+
+  private def getConfiguration = {
+    val configurationProvider: FileConfigurationProvider = new FileConfigurationProvider()
+    val configuration = configurationProvider.loadConfiguration
+    configuration
+  }
+
+  private def waitForAllResponses(configuration: NodeConfiguration, peersThatResponded: ConcurrentLinkedQueue[NetworkPeer]) = {
     val availablePeers = configuration.peers
     repeatWhileWithDelay {
-      peersThatResponded.length < availablePeers.length
+      peersThatResponded.size() < availablePeers.length
     }
-
-    val allResponsesReceived = availablePeers.forall(peerConf => peersThatResponded.exists(
-      peer => {
+    val allResponsesReceived = availablePeers.forall(peerConf => peersThatResponded
+      .stream()
+      .anyMatch(peer => {
         peer.address == peerConf.address && peer.port == peerConf.port
       }))
     assert(allResponsesReceived, "Not all nodes responded!")
@@ -61,7 +77,7 @@ object EntryPoint {
   }
 
 
-  private def buildListener(localPeer: NetworkPeer, messageConsumer: StandardNetworkMessageConsumer) = {
+  private def buildListener(localPeer: NetworkPeer, messageConsumer: NetworkMessageConsumer) = {
     val testListener = NetworkListenerBuilder()
       .withId(localPeer.id)
       .withAddress(localPeer.address)
